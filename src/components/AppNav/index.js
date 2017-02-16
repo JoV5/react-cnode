0aea70d4bea8b091db656a581f8713c638f05fde
JoV5 <360870202@qq.com>
2017-02-16 19:02:48 +0800

    update AppNav

--- src/components/AppNav/index.js
+++ src/components/AppNav/index.js
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from "react";
+import {Link} from 'react-router-dom';
 
 import NavLink from './NavLink';
 
@@ -21,17 +22,28 @@ export default class AppNav extends Component {
     const {appNavIsShow, me} = props;
     const accesstoken = me.get('accesstoken');
     const loginname = me.get('loginname');
+    const avatar_url = me.get('avatar_url');
 
     return (
       <div>
         {appNavIsShow && <div className="page_cover" onClick={handleToggleAppNav}></div>}
         <div className={`app_nav_wrap ${appNavIsShow ? '' : 'hide'}`}>
-          <nav className="app_nav">
+          <div className="app_nav_me">
             {
               accesstoken ?
-                <NavLink to={`/user/${loginname}`} name={loginname} onClick={handleToggleAppNav}/> :
-                <NavLink to="/login" name="登录" onClick={handleToggleAppNav}/>
+                (
+                  <Link to={`/user/${loginname}`} onClick={handleToggleAppNav}>
+                    <img src={avatar_url} className="app_nav_avatar"/>
+                  </Link>
+                ) :
+                (
+                  <Link to="/login">
+                    <button className="app_nav_login_btn" onClick={handleToggleAppNav}>登录</button>
+                  </Link>
+                )
             }
+          </div>
+          <nav className="app_nav">
             <NavLink to="/topics/all" name="全部" onClick={handleToggleAppNav}/>
             <NavLink to="/topics/good" name="精华" onClick={handleToggleAppNav}/>
             <NavLink to="/topics/share" name="分享" onClick={handleToggleAppNav}/>
