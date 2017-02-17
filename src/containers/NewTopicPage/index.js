8d41d33013fcec7d614b13087693e7f0035d3db1
JoV5 <360870202@qq.com>
2017-02-17 13:39:20 +0800

    add NewTopicPage

---
+++ src/containers/NewTopicPage/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export default class NewTopicPage extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>NewTopicPage</h1>
+      </div>
+    );
+  }
+}
